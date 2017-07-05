cdc1b99df7ac2b6d43d0079a716bf5463a18bc3a
Logan Truong <ttluan411@gmail.com>
2017-07-05 12:25:35 -0600

    fixed

--- src/Species.js
+++ src/Species.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react';
 
-class Species extends Component {
+export default function Species(props) {
+    function handleClick(){
+        props.action(props.name);
+    }
+    return (
+        <div>
+            <h5>{props.nane}</h5>
+            <p>{props.status}</p>
+            <button onClick={handleClick}>Feed</button>
+        </div>
+    )
+}
+// class Species extends Component {
     
 
-  handleClick(){
-    this.props.action(this.props.name);
-  }
+//   handleClick(){
+//     this.props.action(this.props.name);
+//   }
 
-    render(){
-          console.log("rendering species");
-          console.log(this.props)
-        return (
-            <div>
-                <h5>{this.props.name}</h5>
-                <p>{this.props.status}</p>
-                <button onClick={this.handleClick.bind(this)}>Feed</button>
-            </div>
-        )
-    }
-}
+//     render(){
+//           console.log("rendering species");
+//           console.log(this.props)
+//         return (
+//             <div>
+//                 <h5>{this.props.name}</h5>
+//                 <p>{this.props.status}</p>
+//                 <button onClick={this.handleClick.bind(this)}>Feed</button>
+//             </div>
+//         )
+//     }
+// }
 
-export default Species
+// export default Species
+
+
